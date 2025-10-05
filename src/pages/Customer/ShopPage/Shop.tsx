f56e914869591292552f227179fa5125d6e1d3c4
BernVein <bernveinbalermo@gmail.com>
2025-10-05 12:19:42 +0800

    add(shop): add item view in shop
    - rough draft, copied from hero UI pro

--- src/pages/Customer/ShopPage/Shop.tsx
+++ src/pages/Customer/ShopPage/Shop.tsx
@@ -1,3 +1,27 @@
+"use client";
+import { ScrollShadow } from "@heroui/react";
+
+import ProductListItem from "./product-list-item";
+import products from "./products";
+
 export default function Shop() {
-	return <></>;
+	return (
+		<div className="flex items-center justify-center min-h-screen">
+			<div className="my-auto flex w-full max-w-7xl flex-col items-center justify-center gap-2">
+				<ScrollShadow
+					className="flex w-full snap-x justify-center gap-6 px-6 py-5"
+					orientation="horizontal"
+					size={20}
+				>
+					{products.map((product) => (
+						<ProductListItem
+							key={product.id}
+							{...product}
+							className="snap-start"
+						/>
+					))}
+				</ScrollShadow>
+			</div>
+		</div>
+	);
 }
