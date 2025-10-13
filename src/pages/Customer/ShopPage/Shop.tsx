f9f651a166a05a4aa074c126686e508ebc296d04
BALERMO FAM <BALERMO FAM@WAYKLASE>
2025-10-13 11:52:12 +0800

    add(Image): add image at the shop page
    - responsive

--- src/pages/Customer/ShopPage/Shop.tsx
+++ src/pages/Customer/ShopPage/Shop.tsx
@@ -1,7 +1,18 @@
 import ShopItems from "./ShopItems";
+import { Image } from "@heroui/react";
+
 export default function Shop() {
 	return (
-		<div>
+		<div className="p-10 flex flex-col gap-10">
+			<div className="h-72 md:h-80 overflow-hidden rounded-2xl">
+				<Image
+					alt="HeroUI hero Image"
+					src="/cover.jpg"
+					className="w-full h-full object-cover"
+					removeWrapper
+				/>
+			</div>
+
 			<ShopItems />
 		</div>
 	);
