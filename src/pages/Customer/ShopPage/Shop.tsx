a7477beb37f41267be7dce2742d8fea9ef3dbed2
BernVein <bernveinbalermo@gmail.com>
2025-10-13 12:41:56 +0800

    feat(navbar): replace baybayani logo with the new one

--- src/pages/Customer/ShopPage/Shop.tsx
+++ src/pages/Customer/ShopPage/Shop.tsx
@@ -3,7 +3,7 @@ import { Image } from "@heroui/react";
 
 export default function Shop() {
 	return (
-		<div className="p-10 flex flex-col gap-10">
+		<div className="p-10 flex flex-col gap-7">
 			<div className="h-72 md:h-80 overflow-hidden rounded-2xl">
 				<Image
 					alt="HeroUI hero Image"
