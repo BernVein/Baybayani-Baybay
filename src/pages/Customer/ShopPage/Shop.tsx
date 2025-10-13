7462c24f38850b5e9c76cc6fc2e3e67c12f1539b
BALERMO FAM <BALERMO FAM@WAYKLASE>
2025-10-13 17:01:05 +0800

    fix(shop): add width responsiveness fix(image header): fix resonsiveness issues

--- src/pages/Customer/ShopPage/Shop.tsx
+++ src/pages/Customer/ShopPage/Shop.tsx
@@ -3,7 +3,7 @@ import ImageHeader from "./ImageHeader";
 
 export default function Shop() {
 	return (
-		<div className="p-5 sm:p-10 flex flex-col gap-7">
+		<div className="p-5 sm:p-10 flex flex-col gap-7 md:w-3/4 mx-auto items-center">
 			<ImageHeader />
 			<ShopItems />
 		</div>
