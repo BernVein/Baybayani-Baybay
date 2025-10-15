84f222f13c864da66ee5100068f1e7208eb0a84e
BALERMO FAM <BALERMO FAM@WAYKLASE>
2025-10-15 15:53:17 +0800

    add(categories): add category in shop
    - add icons for each category
    
    feat(item card): item card typography changed
    
    - sold by is moved beside the price

---
+++ src/pages/Customer/ShopPage/Categories.tsx
@@ -0,0 +1,68 @@
+import {
+	VegetablesOutline,
+	FoodGrains24Regular,
+	FruitsOutline,
+	PoultryLeg,
+	Chili,
+} from "@/components/icons";
+import { ScrollShadow, Button } from "@heroui/react";
+import { useState } from "react";
+
+interface Category {
+	name: string;
+	icon: React.ReactElement;
+}
+
+const cat: Category[] = [
+	{ name: "Vegetables", icon: <VegetablesOutline /> },
+	{ name: "Grains", icon: <FoodGrains24Regular /> },
+	{ name: "Fruits", icon: <FruitsOutline /> },
+	{ name: "Poultry", icon: <PoultryLeg /> },
+	{ name: "Spices", icon: <Chili /> },
+];
+
+export default function Categories() {
+	const [activeCategory, setActiveCategory] = useState<string | null>(null);
+
+	return (
+		<div className="w-full">
+			<p className="text-left md:text-center">Filter Products</p>
+
+			<ScrollShadow
+				orientation="horizontal"
+				className="w-full scroll-smooth p-4"
+			>
+				<div className="flex justify-center items-center gap-5 sm:gap-10 min-w-max mx-auto snap-x snap-mandatory">
+					{cat.map((item) => (
+						<div
+							key={item.name}
+							className="flex items-center flex-shrink-0"
+						>
+							<div className="flex flex-col items-center flex-shrink-0 snap-center">
+								<Button
+									startContent={item.icon}
+									aria-label={item.name}
+									radius="full"
+									color={
+										activeCategory === item.name
+											? "success"
+											: "default"
+									}
+									onPress={() =>
+										setActiveCategory((prev) =>
+											prev === item.name
+												? null
+												: item.name
+										)
+									}
+								>
+									{item.name}
+								</Button>
+							</div>
+						</div>
+					))}
+				</div>
+			</ScrollShadow>
+		</div>
+	);
+}
--- src/pages/Customer/ShopPage/Shop.tsx
+++ src/pages/Customer/ShopPage/Shop.tsx
@@ -1,10 +1,13 @@
 import ShopItems from "@/pages/Customer/ShopPage/ShopItems";
 import ImageHeader from "./ImageHeader";
+import Categories from "./Categories";
 
 export default function Shop() {
 	return (
 		<div className="p-5 sm:p-10 flex flex-col gap-7 md:w-3/4 mx-auto items-center">
 			<ImageHeader />
+			<Categories />
+
 			<ShopItems />
 		</div>
 	);
