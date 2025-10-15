ddd58ff31caa29be6caf7019bec78903cfec344e
BALERMO FAM <BALERMO FAM@WAYKLASE>
2025-10-15 16:50:23 +0800

    add(tag type): add tag type dynamic color

--- src/pages/Customer/ShopPage/Shop.tsx
+++ src/pages/Customer/ShopPage/Shop.tsx
@@ -4,7 +4,7 @@ import Categories from "./Categories";
 
 export default function Shop() {
 	return (
-		<div className="p-5 sm:p-10 flex flex-col gap-7 md:w-3/4 mx-auto items-center">
+		<div className="p-5 sm:p-10 flex flex-col gap-4 md:w-3/4 mx-auto items-center">
 			<ImageHeader />
 			<Categories />
 			<ShopItems />
