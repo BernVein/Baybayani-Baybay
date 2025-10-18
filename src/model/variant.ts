ea1aed410c97bb23d96c934741c44747e53e8d54
BALERMO FAM <BALERMO FAM@WAYKLASE>
2025-10-18 12:54:29 +0800

    refactor(variant and item model): add comment for data types in db

--- src/model/variant.ts
+++ src/model/variant.ts
@@ -5,12 +5,12 @@ export interface Variant {
 	variant_price_wholesale?: number | null;
 	variant_wholesale_item?: number | null;
 	variant_stocks: number;
-	variant_last_updated_stock?: string | null;
-	variant_last_updated_price_retail?: string | null;
+	variant_last_updated_stock: string; // timestamptz
+	variant_last_updated_price_retail?: string | null; // timestamptz
 	variant_last_price_retail?: number;
-	variant_last_updated_price_wholesale?: string | null;
+	variant_last_updated_price_wholesale?: string | null; // timestamptz
 	variant_last_price_wholesale?: number | null;
-	last_updated: string;
+	last_updated: string; // timestamptz
 	is_soft_deleted: boolean;
-	created_at: string;
+	created_at: string; // timestamptz
 }
