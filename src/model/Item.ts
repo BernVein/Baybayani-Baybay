5b11487c15e53f70a30d2f5c60308e03205b2a55
BernVein <bernveinbalermo@gmail.com>
2025-10-18 12:54:29 +0800

    refactor(variant and item model): add comment for data types in db

--- src/model/Item.ts
+++ src/model/Item.ts
@@ -3,12 +3,12 @@ export interface Item {
 	item_id: string;
 	item_category: string;
 	item_title: string;
-	item_img: string[];
+	item_img: string[]; // Join in db, this column doenst exist
 	item_sold_by: string;
 	item_description: string;
 	item_tag?: string | null;
 	is_soft_deleted: boolean;
-	last_updated: string;
-	created_at?: string;
-	item_variants: Variant[];
+	last_updated: string; // timestamptz
+	created_at?: string; // timestamptz
+	item_variants: Variant[]; // Join in db, this column doenst exist
 }
