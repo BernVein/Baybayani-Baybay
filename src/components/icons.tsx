33fd5b63b6eca635ba153d66a30b0f70e1aa726d
BALERMO FAM <BALERMO FAM@WAYKLASE>
2025-10-19 17:54:29 +0800

    fix(information section for product modal): fix issue where the wholesale option can still be selected even the stock is less than the wholesale item
    feat(information section for prodcut modal): add selected variant information

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -361,14 +361,14 @@ export function XIcon(props: IconSvgProps, className: string) {
 			xmlns="http://www.w3.org/2000/svg"
 			fill="none"
 			viewBox="0 0 24 24"
-			stroke-width="1.5"
+			strokeWidth="1.5"
 			stroke="currentColor"
 			className={className}
 			{...props}
 		>
 			<path
-				stroke-linecap="round"
-				stroke-linejoin="round"
+				strokeLinecap="round"
+				strokeLinejoin="round"
 				d="M6 18 18 6M6 6l12 12"
 			/>
 		</svg>
