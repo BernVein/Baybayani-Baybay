40e3c66dc3573131f561b030d088986def63a24d
BernVein <bernveinbalermo@gmail.com>
2025-10-31 23:30:00 +0800

    fix(cart model): fix cart model still usign old cart item model

--- src/model/cart.ts
+++ src/model/cart.ts
@@ -1,9 +1,9 @@
-import { CartItem } from "@/model/cartItemUser";
+import { CartItemUser } from "@/model/cartItemUser";
 
 export interface Cart {
 	cart_id: string;
 	user_id: string;
-	items: CartItem[];
+	items: CartItemUser[];
 	is_soft_deleted: boolean;
 	created_at: string;
 	updated_at: string;
