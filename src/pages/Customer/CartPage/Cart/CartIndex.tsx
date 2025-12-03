10760feb43da87b08a63a365e8a70c7c09175f7d
Bern Vein Balermo <bernveinbalermo@gmail.com>
2025-12-03 22:22:56 +0800

    refactor(checkout modal): checkout modal is now on separated file
    style(cart index): checkout button is disabled when selectedProducts is 0

--- src/pages/Customer/CartPage/Cart/CartIndex.tsx
+++ src/pages/Customer/CartPage/Cart/CartIndex.tsx
@@ -7,17 +7,13 @@ import {
   Skeleton,
   Divider,
   Link,
-  Modal,
-  ModalContent,
-  ModalHeader,
-  ModalBody,
-  ModalFooter,
   useDisclosure,
 } from "@heroui/react";
 import useIsMobile from "@/lib/isMobile";
 import { AddToCart, CartIcon, BaybayaniLogo } from "@/components/icons";
 import CartItem from "@/pages/Customer/CartPage/Cart/CartItem";
 import { useFetchCart } from "@/data/supabase/useFetchCart";
+import CheckoutModalIndex from "../CheckoutModal/CheckoutModalIndex";
 export default function Cart() {
   const [selectedProducts, setSelectedProducts] = useState<string[]>([]);
   const {
@@ -159,6 +155,7 @@ export default function Cart() {
                     color="success"
                     startContent={<AddToCart className="size-6" />}
                     onPress={checkoutModalOnOpen}
+                    isDisabled={selectedProducts.length === 0}
                   >
                     Proceed to Checkout
                   </Button>
@@ -168,116 +165,12 @@ export default function Cart() {
           </Card>
         </div>
       )}
-      <Modal
-        scrollBehavior="inside"
-        isOpen={checkoutModalIsOpen}
-        onOpenChange={checkoutModalOnOpenChange}
-      >
-        <ModalContent>
-          {(onClose) => (
-            <>
-              <ModalHeader className="flex flex-col gap-1">
-                Checkout Summary
-              </ModalHeader>
-              <ModalBody>
-                {selectedItems.length === 0 ? (
-                  <p className="text-default-500 text-center py-4">
-                    No items selected.
-                  </p>
-                ) : (
-                  <div className="space-y-6">
-                    {selectedItems.map((item) => (
-                      <div
-                        key={item.cart_item_user_id}
-                        className="flex items-start gap-4 pb-4 border-b border-default-200"
-                      >
-                        {/* Product image */}
-                        <div className="w-30 h-20 overflow-hidden rounded-md">
-                          <img
-                            src={item.item.item_img[0]}
-                            alt={item.item.item_title}
-                            className="w-full h-full object-cover"
-                          />
-                        </div>
-
-                        {/* Text details */}
-                        <div className="flex flex-col w-full gap-1">
-                          {/* product name */}
-                          <p className="font-semibold text-sm">
-                            {item.variant_snapshot.variant_snapshot_name}
-                          </p>
-
-                          {/* unit price */}
-                          <div className="flex justify-between text-xs text-default-500">
-                            <span className="font-medium text-default-600">
-                              Unit Price
-                            </span>
-                            <span>
-                              ₱
-                              {item.variant_snapshot.variant_snapshot_price_retail.toLocaleString()}
-                            </span>
-                          </div>
-                          {/* price variant */}
-                          <div className="flex justify-between text-xs text-default-500">
-                            <span className="font-medium text-default-600">
-                              Price Variant
-                            </span>
-                            <span>{item.price_variant}</span>
-                          </div>
-
-                          {/* quantity */}
-                          <div className="flex justify-between text-xs text-default-500">
-                            <span className="font-medium text-default-600">
-                              Quantity
-                            </span>
-                            <span>
-                              {item.quantity} {item.item.item_sold_by}
-                              {item.quantity > 1 ? "s" : ""}
-                            </span>
-                          </div>
-
-                          {/* subtotal */}
-                          <div className="flex justify-between text-sm font-semibold mt-1">
-                            <span className="text-default-600">Subtotal</span>
-                            <span>₱{item.subtotal.toLocaleString()}</span>
-                          </div>
-                        </div>
-                      </div>
-                    ))}
-                  </div>
-                )}
-              </ModalBody>
-
-              <ModalFooter className="flex flex-col gap-4">
-                {/* Total Price Section */}
-                <div className="w-full flex justify-between items-center px-1 pb-2 pt-3">
-                  <span className="text-default-600 text-sm sm:text-base font-medium">
-                    Total Price:
-                  </span>
-                  <span className="font-bold text-lg">
-                    ₱{selectedSubtotal.toLocaleString()}
-                  </span>
-                </div>
-
-                {/* Buttons Section */}
-                <div className="w-full flex justify-end gap-2">
-                  <Button color="danger" variant="light" onPress={onClose}>
-                    Close
-                  </Button>
-
-                  <Button
-                    color="success"
-                    onPress={onClose}
-                    startContent={<AddToCart className="size-6" />}
-                  >
-                    Checkout
-                  </Button>
-                </div>
-              </ModalFooter>
-            </>
-          )}
-        </ModalContent>
-      </Modal>
+      <CheckoutModalIndex
+        checkoutModalIsOpen={checkoutModalIsOpen}
+        checkoutModalOnOpenChange={checkoutModalOnOpenChange}
+        selectedItems={selectedItems}
+        selectedSubtotal={selectedSubtotal}
+      />
     </>
   );
 }
--- src/pages/Customer/CartPage/CheckoutModal/CheckoutModalIndex.tsx
+++ src/pages/Customer/CartPage/CheckoutModal/CheckoutModalIndex.tsx
@@ -1,3 +1,4 @@
+import { CartItemUser } from "@/model/cartItemUser";
 import {
   Modal,
   ModalContent,
@@ -5,54 +6,127 @@ import {
   ModalBody,
   ModalFooter,
   Button,
-  useDisclosure,
 } from "@heroui/react";
+import { AddToCart } from "@/components/icons";
 
-export default function CheckoutModalIndex() {
-  const { isOpen, onOpen, onOpenChange } = useDisclosure();
-
+export default function CheckoutModalIndex({
+  checkoutModalIsOpen,
+  checkoutModalOnOpenChange,
+  selectedItems,
+  selectedSubtotal,
+}: {
+  checkoutModalIsOpen: boolean;
+  checkoutModalOnOpenChange: () => void;
+  selectedItems: CartItemUser[];
+  selectedSubtotal: number;
+}) {
   return (
     <>
-      <Button onPress={onOpen}>Open Modal</Button>
       <Modal
-        isDismissable={false}
-        isKeyboardDismissDisabled={true}
-        isOpen={isOpen}
-        onOpenChange={onOpenChange}
+        scrollBehavior="inside"
+        isOpen={checkoutModalIsOpen}
+        onOpenChange={checkoutModalOnOpenChange}
       >
         <ModalContent>
           {(onClose) => (
             <>
               <ModalHeader className="flex flex-col gap-1">
-                Modal Title
+                Checkout Summary
               </ModalHeader>
               <ModalBody>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Nullam pulvinar risus non risus hendrerit venenatis.
-                  Pellentesque sit amet hendrerit risus, sed porttitor quam.
-                </p>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Nullam pulvinar risus non risus hendrerit venenatis.
-                  Pellentesque sit amet hendrerit risus, sed porttitor quam.
-                </p>
-                <p>
-                  Magna exercitation reprehenderit magna aute tempor cupidatat
-                  consequat elit dolor adipisicing. Mollit dolor eiusmod sunt ex
-                  incididunt cillum quis. Velit duis sit officia eiusmod Lorem
-                  aliqua enim laboris do dolor eiusmod. Et mollit incididunt
-                  nisi consectetur esse laborum eiusmod pariatur proident Lorem
-                  eiusmod et. Culpa deserunt nostrud ad veniam.
-                </p>
+                {selectedItems.length === 0 ? (
+                  <p className="text-default-500 text-center py-4">
+                    No items selected.
+                  </p>
+                ) : (
+                  <div className="space-y-6">
+                    {selectedItems.map((item) => (
+                      <div
+                        key={item.cart_item_user_id}
+                        className="flex items-start gap-4 pb-4 border-b border-default-200"
+                      >
+                        {/* Product image */}
+                        <div className="w-30 h-20 overflow-hidden rounded-md">
+                          <img
+                            src={item.item.item_img[0]}
+                            alt={item.item.item_title}
+                            className="w-full h-full object-cover"
+                          />
+                        </div>
+
+                        {/* Text details */}
+                        <div className="flex flex-col w-full gap-1">
+                          {/* product name */}
+                          <p className="font-semibold text-sm">
+                            {item.variant_snapshot.variant_snapshot_name}
+                          </p>
+
+                          {/* unit price */}
+                          <div className="flex justify-between text-xs text-default-500">
+                            <span className="font-medium text-default-600">
+                              Unit Price
+                            </span>
+                            <span>
+                              ₱
+                              {item.variant_snapshot.variant_snapshot_price_retail.toLocaleString()}
+                            </span>
+                          </div>
+                          {/* price variant */}
+                          <div className="flex justify-between text-xs text-default-500">
+                            <span className="font-medium text-default-600">
+                              Price Variant
+                            </span>
+                            <span>{item.price_variant}</span>
+                          </div>
+
+                          {/* quantity */}
+                          <div className="flex justify-between text-xs text-default-500">
+                            <span className="font-medium text-default-600">
+                              Quantity
+                            </span>
+                            <span>
+                              {item.quantity} {item.item.item_sold_by}
+                              {item.quantity > 1 ? "s" : ""}
+                            </span>
+                          </div>
+
+                          {/* subtotal */}
+                          <div className="flex justify-between text-sm font-semibold mt-1">
+                            <span className="text-default-600">Subtotal</span>
+                            <span>₱{item.subtotal.toLocaleString()}</span>
+                          </div>
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                )}
               </ModalBody>
-              <ModalFooter>
-                <Button color="danger" variant="light" onPress={onClose}>
-                  Close
-                </Button>
-                <Button color="primary" onPress={onClose}>
-                  Action
-                </Button>
+
+              <ModalFooter className="flex flex-col gap-4">
+                {/* Total Price Section */}
+                <div className="w-full flex justify-between items-center px-1 pb-2 pt-3">
+                  <span className="text-default-600 text-sm sm:text-base font-medium">
+                    Total Price:
+                  </span>
+                  <span className="font-bold text-lg">
+                    ₱{selectedSubtotal.toLocaleString()}
+                  </span>
+                </div>
+
+                {/* Buttons Section */}
+                <div className="w-full flex justify-end gap-2">
+                  <Button color="danger" variant="light" onPress={onClose}>
+                    Close
+                  </Button>
+
+                  <Button
+                    color="success"
+                    onPress={onClose}
+                    startContent={<AddToCart className="size-6" />}
+                  >
+                    Checkout
+                  </Button>
+                </div>
               </ModalFooter>
             </>
           )}
