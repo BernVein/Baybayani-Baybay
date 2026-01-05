d52d408e8d8823e2ccbaf54977cd10cc75adb5b3
BernVein <bernveinbalermo@gmail.com>
2026-01-05 14:24:36 +0800

    fix(order model): fix order model to reflect the proposed erd

---
+++ src/model/orderItemUser.ts
@@ -0,0 +1,14 @@
+import { Item } from "@/model/Item.js";
+import { VariantSnapshot } from "./variantSnapshot";
+
+export interface OrderItemUser {
+	order_item_user_id: string;
+	item: Item;
+	variant_snapshot: VariantSnapshot; // snapshot of variant at time added
+	price_variant: string;
+	quantity: number;
+	subtotal: number;
+	is_soft_deleted: boolean;
+	created_at: string;
+	updated_at: string;
+}
