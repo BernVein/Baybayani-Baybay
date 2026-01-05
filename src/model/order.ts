2f134c152f62ea994209461baea82858ac28ad74
BernVein <bernveinbalermo@gmail.com>
2026-01-05 14:23:51 +0800

    fix(order model): fix order model to reflect the proposed erd

--- src/model/order.ts
+++ src/model/order.ts
@@ -1,8 +1,8 @@
-import { OrderItem } from "./orderItem";
+import { OrderItemUser } from "./orderItemUser";
 export interface Order {
 	order_id: string;
 	status: "Pending" | "Processing" | "Shipped" | "Delivered" | "Cancelled";
 	created_at: string;
 	total_price: number;
-	items: OrderItem[];
+	items: OrderItemUser[];
 }
--- src/model/orderItem.ts
+++
@@ -1,8 +0,0 @@
-export interface OrderItem {
-	item_id: string;
-	item_name: string;
-	variant_name: string;
-	quantity: number;
-	price: number;
-	image_url: string;
-}
