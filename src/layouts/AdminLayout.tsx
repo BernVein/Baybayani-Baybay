4933fb5331e428a1490a533117e21e7fca4d385b
BernVein <bernveinbalermo@gmail.com>
2026-01-11 20:53:42 +0800

    feat(admin layout): setup admin layouit like in custmomer layout

--- src/layouts/AdminLayout.tsx
+++ src/layouts/AdminLayout.tsx
@@ -1,13 +1,27 @@
 import { Outlet } from "react-router-dom";
+import { Navbar } from "@/components/navbar/navbarDesktop";
+import { NavbarMobile } from "@/components/navbar/navbarMobile";
+import { useState } from "react";
 
 export default function AdminLayout() {
+	const [searchTerm, setSearchTerm] = useState<string | null>(null);
+
 	return (
-		<div className="min-h-screen bg-background text-foreground">
-			{/* Admin Navbar */}
-			<h1>Admin Layout</h1>
-			<main className="p-6">
-				<Outlet />
+		<div className="relative min-h-screen bg-background text-foreground">
+			{/* Top Navbar */}
+			<div className="fixed top-0 left-0 w-full z-50">
+				<Navbar setSearchTerm={setSearchTerm} />
+			</div>
+
+			{/* Page content */}
+			<main className="pt-[64px] pb-[64px]">
+				<Outlet context={{ searchTerm, setSearchTerm }} />
 			</main>
+
+			{/* Bottom Navbar */}
+			<div className="fixed bottom-0 left-0 w-full z-50 sm:hidden">
+				<NavbarMobile />
+			</div>
 		</div>
 	);
 }
