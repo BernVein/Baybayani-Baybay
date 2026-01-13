019655e2eafdd539d6ee9d98f9632940cecec9ed
BernVein <bernveinbalermo@gmail.com>
2026-01-13 21:37:52 +0800

    feat(dashboard): add table for low stock items with avatar

--- src/pages/Admin/Dashboard.tsx
+++ src/pages/Admin/Dashboard.tsx
@@ -1,5 +1,15 @@
 import { DashboardSummary } from "@/pages/Admin/DashboardComponents/DashboardSummary";
-import { Card, CardBody } from "@heroui/react";
+import {
+	Card,
+	CardBody,
+	Table,
+	TableHeader,
+	TableColumn,
+	TableBody,
+	TableRow,
+	TableCell,
+	Avatar,
+} from "@heroui/react";
 import { BasicLineChart } from "./DashboardComponents/BasicLineChart";
 export default function Dashboard() {
 	return (
@@ -14,18 +24,86 @@ export default function Dashboard() {
 				</div>
 			</div>
 			<DashboardSummary />
-			<div className="flex flex-row items-center gap-2">
+			<div className="flex flex-row gap-3 items-stretch h-[380px]">
 				<Card className="w-3/4 p-4">
 					<CardBody>
 						<span className="text-default-500">REVENUE TREND</span>
 						<BasicLineChart />
 					</CardBody>
 				</Card>
-				<Card className="w-1/4">
-					<CardBody>
+
+				<Card className="w-1/4 p-4">
+					<CardBody className="gap-2 overflow-hidden">
 						<span className="text-default-500">
 							LOW STOCK ITEMS
 						</span>
+						<Table
+							isHeaderSticky
+							removeWrapper
+							className="overflow-y-auto"
+						>
+							<TableHeader>
+								<TableColumn>ITEM</TableColumn>
+								<TableColumn>NEEDED</TableColumn>
+							</TableHeader>
+
+							<TableBody emptyContent={"No low stock item."}>
+								<TableRow key="1">
+									<TableCell>
+										<div className="flex flex-row items-center gap-2">
+											<Avatar size="sm" />
+											<span>Tony Reichert</span>
+										</div>
+									</TableCell>
+									<TableCell>5 kg</TableCell>
+								</TableRow>
+								<TableRow key="2">
+									<TableCell>
+										<div className="flex flex-row items-center gap-2">
+											<Avatar size="sm" />
+											<span>Tony Reichert</span>
+										</div>
+									</TableCell>
+									<TableCell>5 kg</TableCell>
+								</TableRow>
+								<TableRow key="3">
+									<TableCell>
+										<div className="flex flex-row items-center gap-2">
+											<Avatar size="sm" />
+											<span>Tony Reichert</span>
+										</div>
+									</TableCell>
+									<TableCell>5 kg</TableCell>
+								</TableRow>
+								<TableRow key="4">
+									<TableCell>
+										<div className="flex flex-row items-center gap-2">
+											<Avatar size="sm" />
+											<span>Tony Reichert</span>
+										</div>
+									</TableCell>
+									<TableCell>5 kg</TableCell>
+								</TableRow>
+								<TableRow key="1">
+									<TableCell>
+										<div className="flex flex-row items-center gap-2">
+											<Avatar size="sm" />
+											<span>Tony Reichert</span>
+										</div>
+									</TableCell>
+									<TableCell>5 kg</TableCell>
+								</TableRow>
+								<TableRow key="2">
+									<TableCell>
+										<div className="flex flex-row items-center gap-2">
+											<Avatar size="sm" />
+											<span>Tony Reichert</span>
+										</div>
+									</TableCell>
+									<TableCell>5 kg</TableCell>
+								</TableRow>
+							</TableBody>
+						</Table>
 					</CardBody>
 				</Card>
 			</div>
