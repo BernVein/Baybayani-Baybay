f69be65fdc25b6a852b20d6ac40907ac485b9cfa
BernVein <bernveinbalermo@gmail.com>
2026-01-14 17:37:19 +0800

    feat(admin tabs): setup admin tabs title and icons

--- src/pages/Admin/Orders.tsx
+++ src/pages/Admin/Orders.tsx
@@ -1,3 +1,20 @@
+import { CartIcon } from "@/components/icons";
+
 export default function Orders() {
-	return <div>Orders</div>;
+	return (
+		<div className="flex flex-col gap-8 p-4">
+			<div className="flex flex-col sm:flex-row sm:items-center justify-between gap-2">
+				<div className="flex flex-row items-center gap-2">
+					<CartIcon className="w-10" />
+					<div className="text-3xl font-semibold">Orders</div>
+				</div>
+				<div className="flex flex-row gap-1 items-center text-muted-foreground">
+					<div className="text-base text-default-500">
+						Logged in as{" "}
+					</div>
+					<div className="text-lg font-semibold">Admin Bern Vein</div>
+				</div>
+			</div>
+		</div>
+	);
 }
--- src/pages/Admin/Products.tsx
+++ src/pages/Admin/Products.tsx
@@ -1,3 +1,20 @@
+import { ProductIcon } from "@/components/icons";
+
 export default function Products() {
-	return <div>Products</div>;
+	return (
+		<div className="flex flex-col gap-8 p-4">
+			<div className="flex flex-col sm:flex-row sm:items-center justify-between gap-2">
+				<div className="flex flex-row items-center gap-2">
+					<ProductIcon className="w-10" />
+					<div className="text-3xl font-semibold">Products</div>
+				</div>
+				<div className="flex flex-row gap-1 items-center text-muted-foreground">
+					<div className="text-base text-default-500">
+						Logged in as{" "}
+					</div>
+					<div className="text-lg font-semibold">Admin Bern Vein</div>
+				</div>
+			</div>
+		</div>
+	);
 }
