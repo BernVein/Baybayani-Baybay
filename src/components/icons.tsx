b4834b09dea4faa6a78f044d82137440cc2ea79e
BernVein <bernveinbalermo@gmail.com>
2026-01-14 20:59:47 +0800

    feat(Orders admin): add table, for now, its for mobile

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -511,6 +511,26 @@ export function MoreIcon(props: IconSvgProps, className: string) {
 		</svg>
 	);
 }
+
+export function MoreIconVertical(props: IconSvgProps, className: string) {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			fill="none"
+			viewBox="0 0 24 24"
+			strokeWidth="1.5"
+			stroke="currentColor"
+			className={className}
+			{...props}
+		>
+			<path
+				strokeLinecap="round"
+				strokeLinejoin="round"
+				d="M12 6.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 18.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z"
+			/>
+		</svg>
+	);
+}
 export function FoodGrains24Regular(props: IconSvgProps) {
 	return (
 		<svg
--- src/pages/Admin/Orders.tsx
+++ src/pages/Admin/Orders.tsx
@@ -1,8 +1,24 @@
-import { CartIcon } from "@/components/icons";
+import { CartIcon, MoreIconVertical } from "@/components/icons";
+import {
+	Table,
+	TableHeader,
+	TableColumn,
+	TableBody,
+	TableRow,
+	TableCell,
+	Avatar,
+	Dropdown,
+	DropdownTrigger,
+	DropdownMenu,
+	DropdownItem,
+	Button,
+	DropdownSection,
+} from "@heroui/react";
 
 export default function Orders() {
 	return (
 		<div className="flex flex-col gap-8 p-4">
+			{/* HEADER ROW */}
 			<div className="flex flex-col sm:flex-row sm:items-center justify-between gap-2">
 				<div className="flex flex-row items-center gap-2">
 					<CartIcon className="w-10" />
@@ -15,6 +31,104 @@ export default function Orders() {
 					<div className="text-lg font-semibold">Admin Bern Vein</div>
 				</div>
 			</div>
+
+			{/* TABLE ROW */}
+			<Table
+				isHeaderSticky
+				selectionMode="single"
+				className="overflow-y-auto h-[500px] w-full"
+			>
+				<TableHeader>
+					<TableColumn>USER</TableColumn>
+					<TableColumn>ITEM</TableColumn>
+					<TableColumn>ACTIONS</TableColumn>
+				</TableHeader>
+
+				<TableBody emptyContent={"No low stock item."}>
+					{Array.from({ length: 20 }).map((_, i) => (
+						<TableRow key={i + 1}>
+							<TableCell>
+								<div className="flex flex-row items-center gap-2">
+									<Avatar size="md" />
+									<span className="text-base">
+										User {i + 1}
+									</span>
+								</div>
+							</TableCell>
+							<TableCell>
+								<div className="flex flex-row gap-2 items-center">
+									<Avatar
+										size="md"
+										color="success"
+										isBordered
+										src={`https://i.pravatar.cc/150?u=a042581f4e49026024${i + 1}`}
+									/>
+									<div className="flex flex-col gap-1 items-start">
+										<span>Item {i + 1}</span>
+										<span className="text-default-500 italic">
+											5 kg
+										</span>
+									</div>
+								</div>
+							</TableCell>
+							<TableCell>
+								<Dropdown>
+									<DropdownTrigger>
+										<Button variant="light" size="sm">
+											<MoreIconVertical className="w-5" />
+										</Button>
+									</DropdownTrigger>
+									<DropdownMenu aria-label="Static Actions">
+										<DropdownSection title="Set Status">
+											<DropdownItem key="processing">
+												<div className="flex items-center gap-2">
+													<span className="w-2 h-2 rounded-full bg-blue-400" />
+													<span>Processing</span>
+												</div>
+											</DropdownItem>
+
+											<DropdownItem key="ready">
+												<div className="flex items-center gap-2">
+													<span className="w-2 h-2 rounded-full bg-yellow-400" />
+													<span>Ready</span>
+												</div>
+											</DropdownItem>
+
+											<DropdownItem key="completed">
+												<div className="flex items-center gap-2">
+													<span className="w-2 h-2 rounded-full bg-green-400" />
+													<span>Completed</span>
+												</div>
+											</DropdownItem>
+
+											<DropdownItem key="cancel">
+												<div className="flex items-center gap-2">
+													<span className="w-2 h-2 rounded-full bg-red-300" />
+													<span className="text-danger">
+														Cancel
+													</span>
+												</div>
+											</DropdownItem>
+										</DropdownSection>
+										<DropdownSection title="Order Date">
+											<DropdownItem
+												key="date"
+												className="pointer-events-none cursor-default"
+											>
+												<div className="flex items-center gap-2">
+													<span>
+														Jan 10, 2025 10:00 AM
+													</span>
+												</div>
+											</DropdownItem>
+										</DropdownSection>
+									</DropdownMenu>
+								</Dropdown>
+							</TableCell>
+						</TableRow>
+					))}
+				</TableBody>
+			</Table>
 		</div>
 	);
 }
