f00932664920788464108eb045746d46fa6cede3
BernVein <bernveinbalermo@gmail.com>
2026-01-18 20:08:29 +0800

    fix(admin pages): force no scroll for all pages in admin except dashboard

--- src/layouts/AdminLayout.tsx
+++ src/layouts/AdminLayout.tsx
@@ -1,10 +1,25 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import { NavbarMobileAdmin } from "@/components/navbar/AdminNav/navbarMobileAdmin";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { SidebarDesktopAdmin } from "@/components/navbar/AdminNav/sidebarDesktopAdmin";
+import useIsMobile from "@/lib/isMobile";
 
 export default function AdminLayout() {
 	const [searchTerm, setSearchTerm] = useState<string | null>(null);
+	const location = useLocation();
+	const isDashboard = location.pathname.toLowerCase().includes("dashboard");
+	const isMobile = useIsMobile();
+
+	useEffect(() => {
+		if (isMobile && !isDashboard) {
+			document.body.style.overflow = "hidden";
+		} else {
+			document.body.style.overflow = "";
+		}
+		return () => {
+			document.body.style.overflow = "";
+		};
+	}, [isMobile, isDashboard]);
 
 	return (
 		<div className="relative min-h-screen bg-background text-foreground">
