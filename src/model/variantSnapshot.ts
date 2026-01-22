29672e9ef39f56021ada54a3e9d915c7e8bc67a9
BernVein <bernveinbalermo@gmail.com>
2026-01-22 22:14:36 +0800

    refactor(variant snapshot model): remove unnecessary comments

--- src/model/variantSnapshot.ts
+++ src/model/variantSnapshot.ts
@@ -1,11 +1,11 @@
 export interface VariantSnapshot {
-	variant_snapshot_id: string;
-	variant_copy_snapshot_id: string;
-	variant_snapshot_name: string;
-	variant_snapshot_price_retail: number;
-	variant_snapshot_price_wholesale?: number | null;
-	variant_snapshot_wholesale_item?: number | null;
-	last_updated: string; // timestamptz
-	is_soft_deleted: boolean;
-	created_at: string; // timestamptz
+    variant_snapshot_id: string;
+    variant_copy_snapshot_id: string;
+    variant_snapshot_name: string;
+    variant_snapshot_price_retail: number;
+    variant_snapshot_price_wholesale?: number | null;
+    variant_snapshot_wholesale_item?: number | null;
+    last_updated: string;
+    is_soft_deleted: boolean;
+    created_at: string;
 }
