430841cad9808537737bad15e8c1db171b971c9e
BernVein <bernveinbalermo@gmail.com>
2026-01-23 23:22:09 +0800

    feat(admin layout): allow overflow on pages cause it causes more problem than solution

--- src/layouts/AdminLayout.tsx
+++ src/layouts/AdminLayout.tsx
@@ -1,41 +1,26 @@
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import { NavbarMobileAdmin } from "@/components/navbar/AdminNav/navbarMobileAdmin";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { SidebarDesktopAdmin } from "@/components/navbar/AdminNav/sidebarDesktopAdmin";
-import useIsMobile from "@/lib/isMobile";
 
 export default function AdminLayout() {
-	const [searchTerm, setSearchTerm] = useState<string | null>(null);
-	const location = useLocation();
-	const isDashboard = location.pathname.toLowerCase().includes("dashboard");
-	const isMobile = useIsMobile();
+    const [searchTerm, setSearchTerm] = useState<string | null>(null);
 
-	useEffect(() => {
-		if (isMobile && !isDashboard) {
-			document.body.style.overflow = "hidden";
-		} else {
-			document.body.style.overflow = "";
-		}
-		return () => {
-			document.body.style.overflow = "";
-		};
-	}, [isMobile, isDashboard]);
+    return (
+        <div className="relative min-h-screen bg-background text-foreground">
+            <div className="fixed left-0 w-full h-full hidden sm:block">
+                <SidebarDesktopAdmin />
+            </div>
 
-	return (
-		<div className="relative min-h-screen bg-background text-foreground">
-			<div className="fixed left-0 w-full h-full hidden sm:block">
-				<SidebarDesktopAdmin />
-			</div>
+            {/* Page content */}
+            <main className="sm:pl-[330px] mb-20 sm:mb-0">
+                <Outlet context={{ searchTerm, setSearchTerm }} />
+            </main>
 
-			{/* Page content */}
-			<main className="sm:pl-[330px] mb-20 sm:mb-0">
-				<Outlet context={{ searchTerm, setSearchTerm }} />
-			</main>
-
-			{/* Bottom Navbar */}
-			<div className="fixed bottom-0 left-0 w-full z-50 sm:hidden">
-				<NavbarMobileAdmin />
-			</div>
-		</div>
-	);
+            {/* Bottom Navbar */}
+            <div className="fixed bottom-0 left-0 w-full z-50 sm:hidden">
+                <NavbarMobileAdmin />
+            </div>
+        </div>
+    );
 }
