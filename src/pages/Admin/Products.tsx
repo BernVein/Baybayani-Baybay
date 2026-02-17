77bc002bcb9f88ae4450ba80de47f943ffaa30ce
BernVein <bernveinbalermo@gmail.com>
2026-02-17 18:59:42 +0800

    feat(product summary admin): add loading skeleton

--- src/pages/Admin/Products.tsx
+++ src/pages/Admin/Products.tsx
@@ -7,49 +7,55 @@ import { ProductTable } from "@/pages/Admin/ProductsComponent/ProductTable";
 import { useFetchProductsUI } from "@/data/supabase/Admin/Products/useFetchProductsUI";
 
 export default function Products() {
-  const [searchQuery, setSearchQuery] = useState<string>("");
-  const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
+	const [searchQuery, setSearchQuery] = useState<string>("");
+	const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
 
-  const { items, allItems, loading, refetch } = useFetchProductsUI(
-    searchQuery,
-    selectedCategories,
-  );
+	const { items, allItems, loading, refetch } = useFetchProductsUI(
+		searchQuery,
+		selectedCategories,
+	);
 
-  useEffect(() => {
-    console.log("Products page rendered with searchQuery:", searchQuery);
-    console.log(
-      "Products page rendered with selectedCategories:",
-      selectedCategories,
-    );
-  }, [searchQuery, selectedCategories]);
+	useEffect(() => {
+		console.log("Products page rendered with searchQuery:", searchQuery);
+		console.log(
+			"Products page rendered with selectedCategories:",
+			selectedCategories,
+		);
+	}, [searchQuery, selectedCategories]);
 
-  return (
-    <div className="flex flex-col gap-8 p-4 h-full">
-      <div className="flex flex-col sm:flex-row sm:items-center justify-between gap-2 shrink-0">
-        <div className="flex flex-row items-center gap-2">
-          <ProductIcon className="w-10" />
-          <div className="text-3xl font-semibold">Products</div>
-        </div>
-        <div className="flex flex-row gap-1 items-center text-muted-foreground">
-          <div className="text-base text-default-500">Logged in as </div>
-          <div className="text-lg font-semibold">Admin Bern Vein</div>
-        </div>
-      </div>
-      <div className="hidden sm:block shrink-0">
-        <ProductSummary items={allItems} />
-      </div>
+	return (
+		<div className="flex flex-col gap-8 p-4 h-full">
+			<div className="flex flex-col sm:flex-row sm:items-center justify-between gap-2 shrink-0">
+				<div className="flex flex-row items-center gap-2">
+					<ProductIcon className="w-10" />
+					<div className="text-3xl font-semibold">Products</div>
+				</div>
+				<div className="flex flex-row gap-1 items-center text-muted-foreground">
+					<div className="text-base text-default-500">
+						Logged in as{" "}
+					</div>
+					<div className="text-lg font-semibold">Admin Bern Vein</div>
+				</div>
+			</div>
+			<div className="hidden sm:block shrink-0">
+				<ProductSummary items={allItems} isLoading={loading} />
+			</div>
 
-      <div className="flex flex-row items-center justify-between shrink-0">
-        <FilterSection
-          searchQuery={searchQuery}
-          selectedCategories={selectedCategories}
-          setSearchQuery={setSearchQuery}
-          setSelectedCategories={setSelectedCategories}
-        />
-      </div>
-      <div className="flex-1 min-h-0">
-        <ProductTable items={items} loading={loading} refetch={refetch} />
-      </div>
-    </div>
-  );
+			<div className="flex flex-row items-center justify-between shrink-0">
+				<FilterSection
+					searchQuery={searchQuery}
+					selectedCategories={selectedCategories}
+					setSearchQuery={setSearchQuery}
+					setSelectedCategories={setSelectedCategories}
+				/>
+			</div>
+			<div className="flex-1 min-h-0">
+				<ProductTable
+					items={items}
+					loading={loading}
+					refetch={refetch}
+				/>
+			</div>
+		</div>
+	);
 }
