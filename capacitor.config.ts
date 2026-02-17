b55cddc24955360dfae065bd69eb34cb44c81bf5
BernVein <bernveinbalermo@gmail.com>
2026-02-17 14:04:44 +0800

    feat(android): add android build option

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.baybayani.app',
+  appName: 'Baybayani',
+  webDir: 'dist'
+};
+
+export default config;
