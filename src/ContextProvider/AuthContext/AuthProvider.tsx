d1376e74ddf6c5829c010f7d9a94e135062aebd7
BernVein <bernveinbalermo@gmail.com>
2026-02-20 10:23:59 +0800

    feat(fetch user): separate fetching user to the auth provider

--- src/ContextProvider/AuthContext/AuthProvider.tsx
+++ src/ContextProvider/AuthContext/AuthProvider.tsx
@@ -1,50 +1,10 @@
-import { createContext, useContext, useEffect, useState } from "react";
-import { supabase } from "@/config/supabaseclient";
+import { createContext, useContext } from "react";
 import { ReactNode } from "react";
-
+import { fetchUser } from "@/data/supabase/General/AuthContext/fetchUser";
 const AuthContext = createContext<any>(null);
 
 export function AuthProvider({ children }: { children: ReactNode }) {
-	const [user, setUser] = useState<any>(null);
-	const [profile, setProfile] = useState<any>(null);
-	const [role, setRole] = useState<string | null>(null);
-	const [loading, setLoading] = useState(true);
-
-	useEffect(() => {
-		const fetchUser = async () => {
-			const {
-				data: { user },
-			} = await supabase.auth.getUser();
-			if (user) {
-				setUser(user);
-
-				const { data: profile } = await supabase
-					.from("User")
-					.select("user_name, user_profile_img_url, user_role")
-					.eq("user_id", user.id)
-					.single();
-
-				setProfile(profile);
-				setRole(profile?.user_role ?? null);
-			} else {
-				setUser(null);
-				setProfile(null);
-				setRole(null);
-			}
-
-			setLoading(false);
-		};
-
-		fetchUser();
-
-		const { data: listener } = supabase.auth.onAuthStateChange(() => {
-			fetchUser();
-		});
-
-		return () => {
-			listener.subscription.unsubscribe();
-		};
-	}, []);
+	const { user, profile, role, loading } = fetchUser();
 
 	return (
 		<AuthContext.Provider value={{ user, profile, role, loading }}>
---
+++ src/data/supabase/General/AuthContext/fetchUser.ts
@@ -0,0 +1,52 @@
+import { useEffect, useState } from "react";
+import { supabase } from "@/config/supabaseclient";
+
+export function fetchUser() {
+	const [user, setUser] = useState<any>(null);
+	const [profile, setProfile] = useState<any>(null);
+	const [role, setRole] = useState<string | null>(null);
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		const fetchUser = async () => {
+			const {
+				data: { user },
+			} = await supabase.auth.getUser();
+			if (user) {
+				setUser(user);
+
+				const { data: profile } = await supabase
+					.from("User")
+					.select("user_name, user_profile_img_url, user_role")
+					.eq("user_id", user.id)
+					.single();
+
+				setProfile(profile);
+				setRole(profile?.user_role ?? null);
+			} else {
+				setUser(null);
+				setProfile(null);
+				setRole(null);
+			}
+
+			setLoading(false);
+		};
+
+		fetchUser();
+
+		const { data: listener } = supabase.auth.onAuthStateChange(() => {
+			fetchUser();
+		});
+
+		return () => {
+			listener.subscription.unsubscribe();
+		};
+	}, []);
+
+	return {
+		user,
+		profile,
+		role,
+		loading,
+	};
+}
