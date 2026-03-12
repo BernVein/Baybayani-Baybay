06ddae098bf69b71b62a3bb77056a6ed35ce3c3a
BernVein <bernveinbalermo@gmail.com>
2026-03-12 14:52:37 +0800

    feat(closing time model): add opening time and closed for  the day boolean

--- src/model/closingTime.ts
+++ src/model/closingTime.ts
@@ -1,5 +1,7 @@
 export interface ClosingTime {
 	closing_time_id: string;
 	closing_time: string; // "HH:MM:SS" (time column from Postgres)
+	opening_time: string; // "HH:MM:SS"
+	is_closed_for_the_day: boolean;
 	updated_at: string;
 }
