599bf867e6e821252f9d2865f12b30c8e2f1fbe8
BernVein <bernveinbalermo@gmail.com>
2026-03-12 15:15:48 +0800

    feat(update closing time): add opening time and opeming time udpate

--- src/data/supabase/Admin/Dashboard/updateClosingTime.ts
+++ src/data/supabase/Admin/Dashboard/updateClosingTime.ts
@@ -1,9 +1,17 @@
 import { supabase } from "@/config/supabaseclient";
 
-export async function updateClosingTime(closingTime: string) {
+export async function updateClosingTime(
+	closingTime: string,
+	openingTime: string,
+	isClosedForTheDay: boolean,
+) {
 	const { error } = await supabase
 		.from("ClosingTime")
-		.update({ closing_time: closingTime })
+		.update({
+			closing_time: closingTime,
+			opening_time: openingTime,
+			is_closed_for_the_day: isClosedForTheDay,
+		})
 		.eq("closing_time_id", "45a6559c-61e5-444d-b8f6-6dc189c52c8a");
 
 	if (error) {
