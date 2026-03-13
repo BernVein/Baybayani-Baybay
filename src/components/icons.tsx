7ecfcbcde01bab570309d5ea09dea58eaf27f11b
BernVein <bernveinbalermo@gmail.com>
2026-03-13 18:51:29 +0800

    feat(icons): add history icon

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1362,3 +1362,25 @@ export const MegaphoneIcon: React.FC<IconSvgProps> = ({
 		<path d="M8 6v8" />
 	</svg>
 );
+export const HistoryIcon = ({
+	size = 24,
+	width,
+	height,
+	...props
+}: IconSvgProps) => (
+	<svg
+		height={size || height}
+		viewBox="0 0 24 24"
+		width={size || width}
+		fill="none"
+		stroke="currentColor"
+		strokeWidth="2"
+		strokeLinecap="round"
+		strokeLinejoin="round"
+		{...props}
+	>
+		<path d="M3 12a9 9 0 1 0 9-9 9.75 9.75 0 0 0-6.74 2.74L3 8" />
+		<path d="M3 3v5h5" />
+		<path d="M12 7v5l4 2" />
+	</svg>
+);
