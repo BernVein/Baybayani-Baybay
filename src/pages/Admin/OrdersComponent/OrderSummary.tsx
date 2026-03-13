2d70d7d43f9c6216b056f8933f70b5ec8366d2a2
BernVein <bernveinbalermo@gmail.com>
2026-03-13 11:44:28 +0800

    feat(order summary): use order summary hook to use real data from db

--- src/pages/Admin/OrdersComponent/OrderSummary.tsx
+++ src/pages/Admin/OrdersComponent/OrderSummary.tsx
@@ -1,63 +1,98 @@
-import { Card, CardBody } from "@heroui/react";
-
+import { Card, CardBody, Skeleton } from "@heroui/react";
 import { PendingIcon, CheckIcon, XIcon } from "@/components/icons";
+import { useOrdersSummary } from "@/data/supabase/Admin/Orders/useOrdersSummary";
 
 export function OrderSummary() {
-  return (
-    <div className="grid grid-cols-1 gap-3 sm:grid-cols-3">
-      <Card className="w-full">
-        <CardBody className="gap-y-3">
-          <span className="text-default-500">TOTAL COMPLETED ORDERS</span>
-          <div className="flex flex-col item-center">
-            <div className="flex flex-row items-center justify-between">
-              <div className="flex flex-row items-center">
-                <span className="text-3xl font-bold">231</span>
-              </div>
+	const { completed, pending, cancelled, loading } = useOrdersSummary();
 
-              <div className="w-12 h-12 flex items-center justify-center rounded-full bg-green-500/70">
-                <CheckIcon className="w-6 h-6 text-white" />
-              </div>
-            </div>
-            <span className="text-default-500">+12.5% vs last month</span>
-          </div>
-        </CardBody>
-        <div className="absolute bottom-0 left-0 w-full h-1 bg-green-500/70 rounded-t-md" />
-      </Card>
-      <Card className="w-full">
-        <CardBody className="gap-y-3">
-          <span className="text-default-500">TOTAL PENDING ORDERS</span>
-          <div className="flex flex-col item-center">
-            <div className="flex flex-row items-center justify-between">
-              <div className="flex flex-row items-center gap-2">
-                <span className="text-3xl font-bold">32</span>
-              </div>
+	return (
+		<div className="grid grid-cols-1 gap-3 sm:grid-cols-3">
+			<Card className="w-full">
+				<CardBody className="gap-y-3">
+					<span className="text-default-500">
+						TOTAL COMPLETED ORDERS
+					</span>
+					<div className="flex flex-col item-center">
+						<div className="flex flex-row items-center justify-between">
+							<div className="flex flex-row items-center">
+								<Skeleton
+									isLoaded={!loading}
+									className="rounded-lg"
+								>
+									<span className="text-3xl font-bold">
+										{completed}
+									</span>
+								</Skeleton>
+							</div>
 
-              <div className="w-12 h-12 flex items-center justify-center rounded-full bg-yellow-500/70">
-                <PendingIcon className="w-6 h-6 text-white" />
-              </div>
-            </div>
-            <span className="text-default-500">5 customers waiting</span>
-          </div>
-        </CardBody>
-        <div className="absolute bottom-0 left-0 w-full h-1 bg-yellow-500/70 rounded-t-md" />
-      </Card>
-      <Card className="w-full">
-        <CardBody className="gap-y-3">
-          <span className="text-default-500">TOTAL CANCELLED ORDERS</span>
-          <div className="flex flex-col item-center">
-            <div className="flex flex-row items-center justify-between">
-              <div className="flex flex-row items-center gap-2">
-                <span className="text-3xl font-bold">14</span>
-              </div>
-              <div className="w-12 h-12 flex items-center justify-center rounded-full bg-red-500/70">
-                <XIcon className="w-6 h-6 text-white" />
-              </div>
-            </div>
-            <span className="text-default-500">+3 vs last month</span>
-          </div>
-        </CardBody>
-        <div className="absolute bottom-0 left-0 w-full h-1 bg-red-500/70 rounded-t-md" />
-      </Card>
-    </div>
-  );
+							<div className="w-12 h-12 flex items-center justify-center rounded-full bg-green-500/70">
+								<CheckIcon className="w-6 h-6 text-white" />
+							</div>
+						</div>
+						<span className="text-default-500">
+							Total of finished orders
+						</span>
+					</div>
+				</CardBody>
+				<div className="absolute bottom-0 left-0 w-full h-1 bg-green-500/70 rounded-t-md" />
+			</Card>
+			<Card className="w-full">
+				<CardBody className="gap-y-3">
+					<span className="text-default-500">
+						TOTAL PENDING ORDERS
+					</span>
+					<div className="flex flex-col item-center">
+						<div className="flex flex-row items-center justify-between">
+							<div className="flex flex-row items-center gap-2">
+								<Skeleton
+									isLoaded={!loading}
+									className="rounded-lg"
+								>
+									<span className="text-3xl font-bold">
+										{pending}
+									</span>
+								</Skeleton>
+							</div>
+
+							<div className="w-12 h-12 flex items-center justify-center rounded-full bg-yellow-500/70">
+								<PendingIcon className="w-6 h-6 text-white" />
+							</div>
+						</div>
+						<span className="text-default-500">
+							Orders waiting for action
+						</span>
+					</div>
+				</CardBody>
+				<div className="absolute bottom-0 left-0 w-full h-1 bg-yellow-500/70 rounded-t-md" />
+			</Card>
+			<Card className="w-full">
+				<CardBody className="gap-y-3">
+					<span className="text-default-500">
+						TOTAL CANCELLED ORDERS
+					</span>
+					<div className="flex flex-col item-center">
+						<div className="flex flex-row items-center justify-between">
+							<div className="flex flex-row items-center gap-2">
+								<Skeleton
+									isLoaded={!loading}
+									className="rounded-lg"
+								>
+									<span className="text-3xl font-bold">
+										{cancelled}
+									</span>
+								</Skeleton>
+							</div>
+							<div className="w-12 h-12 flex items-center justify-center rounded-full bg-red-500/70">
+								<XIcon className="w-6 h-6 text-white" />
+							</div>
+						</div>
+						<span className="text-default-500">
+							Total cancelled transactions
+						</span>
+					</div>
+				</CardBody>
+				<div className="absolute bottom-0 left-0 w-full h-1 bg-red-500/70 rounded-t-md" />
+			</Card>
+		</div>
+	);
 }
