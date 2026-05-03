598a0eadae0a1a0ed88afb3d81490c86ac69f57c
Bern Vein Balermo <bernveinbalermo@gmail.com>
2026-05-03 10:18:20 +0800

    ui(order table desktop): add second to time display

--- src/pages/Admin/OrdersComponent/OrderTableResponsive/OrderTableDesktop.tsx
+++ src/pages/Admin/OrdersComponent/OrderTableResponsive/OrderTableDesktop.tsx
@@ -122,6 +122,7 @@ export function OrderTableDesktop({
 										).toLocaleTimeString("en-US", {
 											hour: "2-digit",
 											minute: "2-digit",
+											second: "2-digit",
 											hour12: true,
 										})}
 									</span>
