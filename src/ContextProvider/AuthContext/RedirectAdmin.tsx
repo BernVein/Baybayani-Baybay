69aa8af636337eadb2c56f3b52d7ffe89ddafa2d
Bern Vein Balermo <bernveinbalermo@gmail.com>
2026-05-11 23:02:09 +0800

    feat(redirect admin): add pageloader when it is loading

--- src/ContextProvider/AuthContext/RedirectAdmin.tsx
+++ src/ContextProvider/AuthContext/RedirectAdmin.tsx
@@ -1,13 +1,14 @@
 import { Navigate } from "react-router-dom";
 import { useAuth } from "@/ContextProvider/AuthContext/AuthProvider";
 import { ReactNode } from "react";
+import FullPageLoader from "@/components/General/FullPageLoader";
 
 export default function RedirectAdmin({ children }: { children: ReactNode }) {
 	const auth = useAuth();
 	const profile = auth?.profile;
 	const loading = auth?.loading;
 
-	if (loading) return null;
+	if (loading) return <FullPageLoader />;
 
 	if (profile?.user_role === "Admin") {
 		return <Navigate to="/admin/dashboard" replace />;
